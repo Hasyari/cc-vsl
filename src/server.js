2df14e1cec494dddc1c5a78a6117eec5e487b90c
Hasyari <hasyari00@gmail.com>
2023-12-12 15:41:52 +0700

    changes port

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use('/news', newsRoutes);
 app.use('/modules', modulesRoutes);
 
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
   console.log('Server started at http://localhost:' + listener.address().port); // Print Out Log Optional
 });
