479d4430b51fd13b47241df6be99ee70810f7612
Hasyari <hasyari00@gmail.com>
2023-12-20 19:46:57 +0700

    make new file

--- src/controller/exercisesController.js
+++ src/controller/exercisesController.js
@@ -1,19 +1,31 @@
-const getExercises = async (req, res) => {
+const uploadImage = require('../utils/uploadImage');
+
+const getPredictSign = async (req, res) => {
   res.status(200).json({
-    success: false,
-    message: 'No controller settings',
+    'message': 'Ini merupakan index',
+    'success': true,
   });
 };
 
-const postPhotoSign = async (req, res) => {
-  res.status(200).json({
-    success: false,
-    message: 'No controller settings',
-  });
+const postPredictSign = async (req, res) => {
+  try {
+    const file = req.file;
+    const uploadedImage = await uploadImage(file, 'vsl-cloud-bucket');
+
+    if (!uploadedImage.success) {
+      console.error('Error uploading image:', uploadedImage.error);
+      return res.status(500).json({error: 'Error uploading image'});
+    }
+    console.log('Image uploaded successfully. URL:', uploadedImage.url);
+    res.status(201).json({message: 'News created successfully'});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({message: 'Internal Server Error'});
+  }
 };
 
 
 module.exports = {
-  getExercises,
-  postPhotoSign,
+  getPredictSign,
+  postPredictSign,
 };
