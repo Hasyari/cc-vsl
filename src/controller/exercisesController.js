6bb92cc072ce25845e203d5bdd3fac22bdbd5116
Hasyari <hasyari00@gmail.com>
2023-12-21 05:59:51 +0700

    updated

--- src/controller/exercisesController.js
+++ src/controller/exercisesController.js
@@ -13,10 +13,8 @@ const postPredictSign = async (req, res) => {
   try {
     const apiUrl = process.env.MODEL_APP_API;
     const formData = new FormData();
-
     const fileBlob = new Blob([req.file.buffer], {type: req.file.mimetype});
 
-    // Append the Blob to the FormData object
     formData.append('file', fileBlob, req.file.originalname);
 
     const response = await axios.post(apiUrl, formData, {
@@ -27,8 +25,13 @@ const postPredictSign = async (req, res) => {
 
     res.json(response.data);
   } catch (error) {
-    console.error('Error uploading file:', error.message);
-    res.status(500).json({error: 'Internal Server Error'});
+    console.error('Error forwarding file:', error.response?.data || error.message);
+
+    if (error.response) {
+      res.status(error.response.status).json({error: error.response.data});
+    } else {
+      res.status(500).json({error: 'File forwarding failed'});
+    }
   }
 };
 
