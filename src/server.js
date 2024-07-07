354ce2677e4d216bfe874eab04a6f3363114b288
Hasyari <hasyari00@gmail.com>
2024-07-07 22:36:45 +0700

    changes

--- src/server.js
+++ src/server.js
@@ -7,22 +7,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const index = require('./routes/index');
-const newsRoutes = require('./routes/news');
-const modulesRoutes = require('./routes/modules');
-const exerciseRoutes = require('./routes/exercises');
-
 app.use(cors());
-
 app.use(bodyParser.json());
-// Parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(cors());
-app.use('/', index);
-app.use('/news', newsRoutes);
-app.use('/modules', modulesRoutes);
-app.use('/exercises', exerciseRoutes);
 
 const listener = app.listen(process.env.PORT || 8080, () => {
   console.log('Your app is listening on port ' + listener.address().port);
